d266aa9b4fbaa81d6a9ad2f4940df951af7e3524
FRAmyZ010 <tadzyz010@gmail.com>
2025-08-23 00:55:36 +0700

    feat: Add password API

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ const con = require('./db');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.get('/password/:raw',(req,res)=>{
+    const raw = req.params.raw;
+    bcrypt.hash(raw,10,(err,hash)=>{
+        if(err){
+            return res.status(500).send('Error creating password!');
+        }
+        res.send(hash);
+    });
+});
 
 app.listen(3000,()=>{
     console.log('Server 🤑 is running on port 3000 ✅');
